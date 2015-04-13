ab3edc61ac9b649eb5af3a1ba501780aab8d336b
Kevin Haferkamp <me@kevinhaferkamp.com>
2015-04-13 19:27:56 +0200

    - Models "Chef" and "Recipe" with Validations - Added tests for Model "Chef" and "Recipe"

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,2 +1,10 @@
 class Chef < ActiveRecord::Base
+  before_save { self.email = email.downcase }
+
+  validates :chefname, presence: true, length: {minimum: 3, maximum: 50}
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: {maximum: 255},
+            uniqueness: {case_sensitive: false}, format: {with: VALID_EMAIL_REGEX}
+
 end
--- test/models/chef_test.rb
+++ test/models/chef_test.rb
@@ -1,7 +1,67 @@
 require 'test_helper'
 
 class ChefTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @chef = Chef.create(chefname: "john", email: "john@example.com")
+  end
+
+  test "chef should be valid" do
+    assert @chef.valid?
+  end
+
+  test "chefname should be present" do
+    @chef.chefname= " "
+
+    assert_not @chef.valid?
+  end
+
+  test "chefname should not be too long" do
+    @chef.chefname= "a"*51
+
+    assert_not @chef.valid?
+  end
+
+  test "chefname should not be too short" do
+    @chef.chefname= "a"
+
+    assert_not @chef.valid?
+  end
+
+  test "email should be present" do
+    @chef.email= " "
+
+    assert_not @chef.valid?
+  end
+
+  test "email should be unique" do
+    dup_chef = @chef.dup
+    dup_chef.email = @chef.email
+    @chef.save
+
+    assert_not dup_chef.valid?
+  end
+
+  test "email should be a valid email" do
+    @chef.email= "email"
+
+    assert_not @chef.valid?
+  end
+
+  test "email should accept whitelist entries" do
+    valid_addresses = %w[john@example.com will.wills@hoster.de master.yoda@force.net]
+
+    valid_addresses.each do |val|
+      @chef.email = val
+      assert @chef.valid?, "#{val.inspect} should be valid"
+    end
+  end
+
+  test "email should reject blacklist entries" do
+    not_valid_addresses = %w[john@example @hoster.de .net]
+
+    not_valid_addresses.each do |val|
+      @chef.email = val
+      assert_not @chef.valid?, "#{val.inspect} should not be valid"
+    end
+  end
 end
