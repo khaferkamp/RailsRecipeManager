f4b310d2c6ec1bf5039bcca3e1a79a1acd72b41e
Kevin Haferkamp <me@kevinhaferkamp.com>
2015-04-20 19:53:11 +0200

    Added pagination

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,7 @@
 class RecipesController < ApplicationController
   def index
-    @recipes = Recipe.all
+    @recipes = Recipe.paginate(page: params[:page], per_page: 5)
+    #Recipe.all.sort_by { |likes| likes.thumbs_up_total}.reverse
   end
 
   def show
@@ -39,6 +40,21 @@ class RecipesController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def like
+    #TODO: Hier ist die ID noch hartkodiert! Bitte ändern!
+    @chef = Chef.find(2)
+    @recipe = Recipe.find(params[:id])
+
+    like = Like.create(like: params[:like], recipe: @recipe, chef: @chef)
+    if like.valid?
+      flash[:success] = "Your selection was successful!"
+    else
+      flash[:warning] = "You can only vote a recipe once!"
+    end
+
+    redirect_to :back
   end
 
   private
